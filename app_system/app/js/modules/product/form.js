bf6a5a0ace07f193750c157fedc479d8286f9640
Alliances <alliances009@gmail.com>
2019-01-06 22:28:49 +0700

    reload after update product

--- app_system/app/js/modules/product/form.js
+++ app_system/app/js/modules/product/form.js
@@ -168,9 +168,15 @@ $(document).ready(function() {
 				try
 				{
 					if (result.success)
-					{
-						swal("Information",result.msg,"success");
+					{						
 						me.reset_form();
+						swal({
+				            title: "Information",
+				            text: result.msg,
+				            type: "success"
+				        }).then(function() {
+				            window.location.reload();
+				        });						
 					}
 					else
 					{
